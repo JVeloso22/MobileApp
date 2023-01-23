a26315a25ac1222af3bf25f60181739280321c08
JVeloso22 <jveloso@egapi.pt>
2023-01-23 12:51:40 +0000

    Header Update

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroPage } from '../intro/intro.page';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'intro',
+    component: IntroPage,
   }
 ];
 
