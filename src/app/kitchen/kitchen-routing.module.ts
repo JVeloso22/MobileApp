30293096b0472766c1f3f52c416cbe364040a16b
JVeloso22 <jveloso@egapi.pt>
2023-01-31 09:52:25 +0000

    Routes Kitchen Page

--- src/app/kitchen/kitchen-routing.module.ts
+++ src/app/kitchen/kitchen-routing.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlindsComponent } from './devices/blinds/blinds.component';
+import { FansComponent } from './devices/fans/fans.component';
+import { LightsComponent } from './devices/lights/lights.component';
+import { TempComponent } from './devices/temp/temp.component';
 
 import { KitchenPage } from './kitchen.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: KitchenPage
-  }
+    component: KitchenPage,
+    children: [
+      {
+        path:'lights',
+        component: LightsComponent,
+      },
+      {
+        path:'temp',
+        component: TempComponent,
+      },
+      {
+        path:'fans',
+        component: FansComponent,
+      },
+      {
+        path:'blinds',
+        component: BlindsComponent,
+      }
+    ],
+  },
 ];
 
 @NgModule({
