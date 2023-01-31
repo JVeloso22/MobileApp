eba1086c0b5f8baf1ae81d7db22bfd5bd62b8a5f
JVeloso22 <jveloso@egapi.pt>
2023-01-31 17:24:24 +0000

    Devices Kitchen & Bedroom

--- src/app/bedroom/bedroom-routing.module.ts
+++ src/app/bedroom/bedroom-routing.module.ts
@@ -2,12 +2,34 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BedroomPage } from './bedroom.page';
+import { BlindsComponent } from './devices/blinds/blinds.component';
+import { FansComponent } from './devices/fans/fans.component';
+import { LightsComponent } from './devices/lights/lights.component';
+import { TempComponent } from './devices/temp/temp.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: BedroomPage
-  }
+    component: BedroomPage,
+    children: [
+      {
+        path: 'lights',
+        component: LightsComponent
+      },
+      {
+        path: 'temp',
+        component: TempComponent
+      },
+      {
+        path: 'fans',
+        component: FansComponent
+      },
+      {
+        path: 'blinds',
+        component: BlindsComponent
+      },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/bedroom/bedroom.module.ts
+++ src/app/bedroom/bedroom.module.ts
@@ -7,6 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { BedroomPageRoutingModule } from './bedroom-routing.module';
 
 import { BedroomPage } from './bedroom.page';
+import { BlindsComponent } from './devices/blinds/blinds.component';
+import { FansComponent } from './devices/fans/fans.component';
+import { LightsComponent } from './devices/lights/lights.component';
+import { TempComponent } from './devices/temp/temp.component';
 
 @NgModule({
   imports: [
@@ -15,6 +19,6 @@ import { BedroomPage } from './bedroom.page';
     IonicModule,
     BedroomPageRoutingModule
   ],
-  declarations: [BedroomPage]
+  declarations: [BedroomPage, BlindsComponent, FansComponent, LightsComponent, TempComponent]
 })
 export class BedroomPageModule {}
