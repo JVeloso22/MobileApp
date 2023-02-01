1bb6466ad3328b2ad41af7043470db5ca91b7876
JVeloso22 <jveloso@egapi.pt>
2023-02-01 09:51:08 +0000

    Kitchen Page Update

--- src/app/kitchen/kitchen.page.ts
+++ src/app/kitchen/kitchen.page.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KitchenPage implements OnInit {
 
+  lights: boolean = false
+  temp: boolean = false
+  fans: boolean = false
+  blinds: boolean = false
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickLights() {
+    this.lights = true
+    this.temp = false
+    this.fans = false
+    this.blinds = false
+  }
+
+  clickTemp() {
+    this.temp = true
+    this.lights = false
+    this.fans = false
+    this.blinds = false
+  }
+
+  clickFans() {
+    this.fans = true
+    this.lights = false
+    this.blinds = false
+    this.temp = false
+  }
+
+  clickBlinds() {
+    this.blinds = true
+    this.fans = false
+    this.lights = false
+    this.temp = false
+  }
 }
