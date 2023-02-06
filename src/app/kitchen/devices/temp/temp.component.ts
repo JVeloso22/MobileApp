5726efbf3c91e5fbb589c3f869764129e93e1b3c
JVeloso22 <jveloso@egapi.pt>
2023-02-06 11:51:56 +0000

    Page Temp

--- src/app/kitchen/devices/temp/temp.component.ts
+++ src/app/kitchen/devices/temp/temp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-temp',
@@ -7,8 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TempComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {}
 
+  activeMessage = "Air Conditioning ON"
+  cheked: boolean = false
+
+  pinFormatter(value: number) {
+    return `${value}%`;
+  }
+
+  async presentToast(position: 'bottom') {
+
+    const toast = await this.toastController.create({
+      message: this.activeMessage,
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  clickAir() {
+    if (this.activeMessage === "Air Conditioning ON") {
+      this.activeMessage = "Air Conditioning OFF"
+      this.cheked = true
+    } else {
+      this.activeMessage = "Air Conditioning ON"
+      this.cheked = false
+    }
+  }
 }
