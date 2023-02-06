b5bdb92ff1bea13b848c179328d1c2f80fedde97
JVeloso22 <jveloso@egapi.pt>
2023-02-06 14:58:02 +0000

    Kitchen Devices Update

--- src/app/kitchen/devices/blinds/blinds.component.ts
+++ src/app/kitchen/devices/blinds/blinds.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-blinds',
@@ -7,8 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlindsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   ngOnInit() {}
+
+  activeMessage = "Fans ON"
+  cheked: boolean = false
+
+  pinFormatter(value: number) {
+    return `${value}%`;
+  }
+
+  async presentToast(position: 'bottom') {
+
+    const toast = await this.toastController.create({
+      message: this.activeMessage,
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+  clickAir() {
+    if (this.activeMessage === "Fans ON") {
+      this.activeMessage = "Fans OFF"
+      this.cheked = true
+    } else {
+      this.activeMessage = "Fans ON"
+      this.cheked = false
+    }
+  }
 
 }
